41069a101c5131adc8f01c607839255145e64f9c
qcts33 <qcthreestones@gmail.com>
2023-02-23 17:16:43 +0800

    fix bug

--- .github/actions/update-version/update-version.py
+++ .github/actions/update-version/update-version.py
@@ -1,8 +1,7 @@
 import os
 import re
 
-
-with open('PKGBUILD') as fp:
+with open("PKGBUILD") as fp:
     for line in fp.readlines():
         line = line.strip()
         current_build_number = re.search(r"^_pkgbuildnumber=(.+)$", line)
@@ -13,32 +12,46 @@ with open('PKGBUILD') as fp:
     else:
         raise ValueError("_pkgbuildnumber not found")
 
-latest_version = os.environ['INPUT_VERSION']
-latest_build_number = os.environ['INPUT_BUILD_NUMBER']
-latest_hash_x86_64 = os.environ['INPUT_SHA256_x86_64']
+latest_version = os.environ["INPUT_VERSION"]
+latest_build_number = os.environ["INPUT_BUILD_NUMBER"]
+latest_hash_x86_64 = os.environ["INPUT_SHA256_x86_64"]
 
-print(f'Current build number: {current_build_number}')
-print(f'Latest build number: {latest_build_number}')
-print(f'Latest version: {latest_version}')
-print(f'{latest_version}+{latest_build_number} x86_64 SHA256: {latest_hash_x86_64}')
+print(f"Current build number: {current_build_number}")
+print(f"Latest build number: {latest_build_number}")
+print(f"Latest version: {latest_version}")
+print(f"{latest_version}+{latest_build_number} x86_64 SHA256: {latest_hash_x86_64}")
 
 if latest_build_number.isdigit() is False:
-    print('Latest build number is invalid')
+    print("Latest build number is invalid")
     exit(1)
 
-if ' ' in latest_version or '-' in latest_version:
-    print('Latest version is invalid')
+if " " in latest_version or "-" in latest_version:
+    print("Latest version is invalid")
     exit(1)
 
-with open('PKGBUILD') as fp:
+with open("PKGBUILD") as fp:
     contents = fp.read()
 
 if current_build_number != latest_build_number:
-    contents = re.sub(r"^pkgrel=.+$", 'pkgrel=1', contents, flags=re.MULTILINE)
+    contents = re.sub(r"^pkgrel=.+$", "pkgrel=1", contents, flags=re.MULTILINE)
 
-contents = re.sub(r"^_pkgbuildnumber=.+$", f'_pkgbuildnumber={latest_build_number}', contents, flags=re.MULTILINE)
-contents = re.sub(r"^_pkgversion=.+$", f'_pkgversion={latest_version}', contents, flags=re.MULTILINE)
-contents = re.sub(r"(sha256sums_x86_64=\(\n  ').+'\n", f"\g<1>{latest_hash_x86_64}'\n", contents)
+contents = re.sub(
+    r"^_pkgbuildnumber=.+$",
+    f"_pkgbuildnumber={latest_build_number}",
+    contents,
+    flags=re.MULTILINE,
+)
+contents = re.sub(
+    r"^_pkgversion=.+$",
+    f"_pkgversion={latest_version}",
+    contents,
+    flags=re.MULTILINE,
+)
+contents = re.sub(
+    r"(sha256sums_x86_64=\(\n  ').+'\n",
+    f"\g<1>{latest_hash_x86_64}'\n",
+    contents,
+)
 
-with open('PKGBUILD', 'w') as fp:
+with open("PKGBUILD", "w") as fp:
     fp.write(contents)
